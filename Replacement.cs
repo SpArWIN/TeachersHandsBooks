e030552e8cde53aa5f391748f9b02565c1be2edc
Николай Балыкин <nikola10www@mail.ru>
2023-11-26 13:45:36 +0300

    Saved

---
+++ Replacement.cs
@@ -0,0 +1,33 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeachersHandsBooks.Core;
+
+namespace TeachersHandsBooks
+{
+    public partial class Replacement : MaterialForm
+    {
+        private ThemeSettings themeSettings;
+        public Replacement(ThemeSettings theme)
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            this.themeSettings = theme;
+
+        }
+
+        private void Replacement_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
